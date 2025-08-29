678a9fe2b55f0d40e7dbd68ca240de546ad026fc
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-08-29 11:41:08 +0900

    add middleware

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ import express from 'express'
 
 const app = express();
 
-app.get('/',(req,res)=>{
-  res.send('res');
+//middle ware
+const middleware = (req,res,next) =>{
+  console.log(`middleware ${new Date()} ${req.method} ${req.url}`)
+  next();//next middleware
+}
+
+app.use(middleware);//middlewareの登録
+//middlewareはhttpリクエストの度に呼び出される
+
+app.get('/',(req,res)=>{//第一引数:request object,第二引数はresponse object
+  res.send('GET res');
 })
 
 app.listen(3000,()=>{
