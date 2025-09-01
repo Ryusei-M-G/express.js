30a2c1c16d7c45814664c67765ddddaf7cfb68ef
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-09-01 14:40:45 +0900

    con

--- dbcon.js
+++ dbcon.js
@@ -1,7 +1,34 @@
-import pg from 'pg';
+import { Client } from 'pg';
 
-const dbConnection = () =>{
-  
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: '1234',
+  database: 'postgres',
+});
+
+const insertQuery = `
+    INSERT INTO tables (users, text) 
+    VALUES ($1, $2)
+    ON CONFLICT (users) DO NOTHING;
+`;
+
+const dbConnection = async () => {
+  try {
+    await client.connect();
+    console.log('sucsess connection');
+    const values = ['sample user', 'sample text'];
+      await client.query(insertQuery, values);
+
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.end();
+    console.log('disconnect');
+  }
 }
+
+dbConnection();
 
 export default dbConnection;
