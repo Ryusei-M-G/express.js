c115c49d0985f8e262a5f66adeeaf6c6e24b3d65
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-09-02 14:46:45 +0900

    add fetchDB function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,17 +8,17 @@ function App() {
   const [, setIsLoading] = useState(false);
   const [serverUrl,] = useState('http://localhost:3000');
   const [data, setData] = useState('sample text');
+  const [DBData, setDBData] = useState([]);
 
   const fetchData = async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch(serverUrl);
+      const response = await fetch(`${serverUrl}/wt`);
       if (!response.ok) {
         throw new Error(`HTTPエラー: ${response.status}`);
       }
       const jsonData = await response.json();
-      console.log(jsonData);
       setData(jsonData);
     } catch (e) {
       setError(e.message);
@@ -31,41 +31,60 @@ function App() {
     fetchData();
   }
 
-  const handleAdd =async(value1,value2) =>{
-    const postData ={
-      item1:value1,
-      item2:value2,
+  const handleAdd = async (value1, value2) => {
+    const postData = {
+      item1: value1,
+      item2: value2,
     };
 
     setIsLoading(true);
     setError(null);
 
-    try{
-      const res = await fetch(`${serverUrl}/add`,{
+    try {
+      const res = await fetch(`${serverUrl}/add`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(postData),
       });
-      if(!res.ok){
-        throw new Error('HTTPerror',res.status);
+      if (!res.ok) {
+        throw new Error('HTTPerror', res.status);
       }
 
       const result = await res.json();
       console.log(`res: ${result}`);
 
       fetchData();
-    }catch(e){
+    } catch (e) {
       setError(e.message);
       console.log(`erroe:${e}`);
-    }finally{
+    } finally {
       setIsLoading(false);
+      fetchhandle();
     }
   }
 
+  const fetchhandle = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(`${serverUrl}/db`);
+      if (!response.ok) {
+        throw new Error(`HTTPエラー: ${response.status}`);
+      }
+      const jsonData = await response.json();
+      console.log(jsonData);
+      setDBData(jsonData);
+    } catch (e) {
+      setError(e.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
   useEffect(() => {
     fetchData();
+    fetchhandle();
   }, []);
 
   return (
@@ -73,7 +92,12 @@ function App() {
       <button className='border rounded-2xl p-2 m-4' onClick={eventHandle}>button</button>
       <Temp data={data}></Temp>
       <Form onAdd={handleAdd}></Form>
-      
+      <button type="button" className="cursor-grab border rounded-2xl pr-4 pl-4 p-1  text-2xl" onClick={fetchhandle}>fetch</button>
+      <div>{DBData.map((e)=>{
+        return(<li key={e.id}>
+          ID: {e.id} - Item1: {e.users}, Item2: {e.text}
+        </li>); 
+      })}</div>
     </div>
   );
 }
